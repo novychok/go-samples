bd2b06d57ac80e92bbf0f9d25f2062e26f0a8d8d
Eric haroyan <haroyan.eric@gmail.com>
2023-11-26 16:58:44 +0400

    example of how to use values of ctx, retrieve and send, to manage errros

--- context01/server/server.go
+++ context01/server/server.go
@@ -3,24 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
-	"math"
+	"math/rand"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	doStuff2(ctx)
+	if err := userRegistration(ctx); err != nil {
+		fmt.Println(err)
+	}
 }
 
-func doStuff2(ctx context.Context) {
-	timer := time.Now()
+func userRegistration(ctx context.Context) error {
+	id := 1 + rand.Intn(5)
+	ctx = context.WithValue(ctx, "userId", id)
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
 	select {
 	case <-ctx.Done():
-		fmt.Println("Time is up: ", math.Round(time.Since(timer).Seconds()))
-		return
-	case <-time.After(8 * time.Second):
-		fmt.Println("The task is complited: ", math.Round(time.Since(timer).Seconds()))
+		return fmt.Errorf("time is up for registration: %v", ctx.Err())
+	case <-time.After(1 * time.Second):
+		msg := fmt.Sprintf("registration for userId[%d] is done, proceed user login\n", id)
+		if err := userLogin(ctx, msg); err != nil {
+			return err
+		}
+		return nil
+	}
+
+}
+
+func userLogin(ctx context.Context, msg string) error {
+	getCtxID, ok := ctx.Value("userId").(int)
+	if !ok {
+		fmt.Printf("wrong custing?\n")
+		return nil
+	}
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("time is up for userId[%d] login", getCtxID)
+	case <-time.After(2 * time.Second):
+		fmt.Print(msg)
+		return nil
 	}
 }
