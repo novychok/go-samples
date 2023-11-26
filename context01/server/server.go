9d2c8ff4c5c73a3fd05b73e212a25e842d8fbf2e
Eric haroyan <haroyan.eric@gmail.com>
2023-11-26 15:58:03 +0400

    my better example than in medium article

--- context01/server/server.go
+++ context01/server/server.go
@@ -3,55 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"net/http"
-	"sync"
+	"math"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
 	defer cancel()
 
-	wg := &sync.WaitGroup{}
-	urlsch := make(chan string)
-
-	urls := []string{
-		"https://google.com",
-		"https://google.com",
-		"https://google.com",
-	}
-
-	for _, url := range urls {
-		wg.Add(1)
-		go fetchAPI(ctx, url, urlsch, wg)
-	}
-
-	go func() {
-		wg.Wait()
-		close(urlsch)
-	}()
-
-	for v := range urlsch {
-		fmt.Println(v)
-	}
-
+	doStuff2(ctx)
 }
 
-func fetchAPI(ctx context.Context, url string, urslch chan<- string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
+func doStuff2(ctx context.Context) {
+	timer := time.Now()
+	select {
+	case <-ctx.Done():
+		fmt.Println("Time is up: ", math.Round(time.Since(timer).Seconds()))
 		return
+	case <-time.After(8 * time.Second):
+		fmt.Println("The task is complited: ", math.Round(time.Since(timer).Seconds()))
 	}
-	// time.Sleep(3 * time.Second)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	urslch <- fmt.Sprintf("Response from: %s %d", url, resp.StatusCode)
 }
